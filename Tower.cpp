524eeeb7310b1d21fdbc1a5e0ecec673937c9a04
AronBaracs <aron.baracs@gmail.com>
2019-03-28 18:45:18 +0100

    added toweread func, fixed locations returning

--- Tower.cpp
+++ Tower.cpp
@@ -19,8 +19,72 @@ void Tower::setTowerID(int towerID) {
     Tower::towerID = towerID;
 }
 
-County Tower::getTowerLocation() const {
-    return towerLocation;
+std::string Tower::getTowerLocation() const {
+    std::string location;
+    switch
+            (towerLocation) {
+        case County::INVALID:
+            location = "Invalid";
+            break;
+        case County::BEKESCSABA :
+            location = "Bekescsaba";
+            break;
+        case County::BUDAPEST :
+            location = "Budapest";
+            break;
+        case County::DEBRECEN :
+            location = "Debrecen";
+            break;
+        case County::SZOMBATHELY :
+            location = "Szombathely";
+            break;
+        case County::GYOR :
+            location = "Gyor";
+            break;
+        case County::ZALAEGERSZEG :
+            location = "Zalaegerszeg";
+            break;
+        case County::VESZPREM:
+            location = "Veszprem";
+            break;
+        case County::KAPOSVAR :
+            location = "Kaposvar";
+            break;
+        case County::PECS :
+            location = "Pecs";
+            break;
+        case County::SZEKSZARD:
+            location = "Szekszard";
+            break;
+        case County::SZEKESFEHERVAR:
+            location = "Szekesfehervar";
+            break;
+        case County::TATABANYA :
+            location = "Tatabanya";
+            break;
+        case County::KECSKEMET :
+            location = "Kecskemet";
+            break;
+        case County::SZEGED:
+            location = "Szeged";
+            break;
+        case County::SZOLNOK :
+            location = "Szolnok";
+            break;
+        case County::SALGOTARJAN:
+            location = "Salgotarjan";
+            break;
+        case County::EGER :
+            location = "Eger";
+            break;
+        case County::NYIREGYHAZA :
+            location = "Nyiregyhaza";
+            break;
+        case County::MISKOLC :
+            location = "Miskolc";
+            break;
+    }
+    return location;
 }
 
 void Tower::setTowerLocation(County towerLocation) {
--- main.cpp
+++ main.cpp
@@ -9,13 +9,14 @@
 #include "Tower.h"
 #include "Location.h"
 
-std::vector<Person> readFromFile(std::string fileName);
+std::vector<Person> readFromPersonFile(std::string fileName);
+std::vector<Tower> readFromTowerFile(std::string fileName);
 
 int main() {
 
     try {
-        std::vector<Person> person = readFromFile("../person.txt");
-
+        std::vector<Person> person = readFromPersonFile("../person.txt");
+        std::vector<Tower> towerVector = readFromTowerFile("../tower.txt");
     } catch (int e) {
         if (e == 2) {
             std::cout << "person.txt could not be opened" << std::endl;
@@ -28,14 +29,11 @@ int main() {
     Person person1("Nrtkh");
     person1.setPersonLocation(County::GYOR);
 
-    if(person1.getPersonLocation() == tower1.getTowerLocation()){
-        std::cout << "jeeeejejejejjeeee" << std::endl;
-    }
 
     return 0;
 }
 
-std::vector<Person> readFromFile(std::string fileName){
+std::vector<Person> readFromPersonFile(std::string fileName){
     std::vector<Person> person;
     std::ifstream file(fileName);
     if (file.is_open()) {
@@ -112,9 +110,92 @@ std::vector<Person> readFromFile(std::string fileName){
     }
 
     for (int i = 0; i <person.size() ; ++i) {
-        std::cout << i << ":" << person[i].getName() << ", " << person[i].getNumber() << ", "  << std::endl;
+        std::cout << i << ":" << person[i].getName() << ", " << person[i].getNumber() << ", " <<person[i].getPersonLocation() << std::endl;
     }
 
     file.close();
     return person;
 }
+
+std::vector<Tower> readFromTowerFile(std::string fileName){
+    std::vector<Tower> towerVector;
+    std::ifstream file(fileName);
+    if (file.is_open()) {
+        std::cout << "Open\n" << std::endl;
+    } else {
+        std::cout << "Your file is closed" << std::endl;
+        throw 2;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+
+        std::istringstream stringStream(line);
+
+        Tower tower1;
+
+        std::string id;
+        std::getline(stringStream, id, ',');
+
+        int towerID = std::stoi(id);
+        tower1.setTowerID(towerID);
+
+        std::string county;
+        std::getline(stringStream, county, ',');
+
+        if(county == "Szombathely"){
+            tower1.setTowerLocation(County::SZOMBATHELY);
+        } else if(county == "Gyor"){
+            tower1.setTowerLocation(County::GYOR);
+        } else if(county == "Zalaegerszeg"){
+            tower1.setTowerLocation(County::ZALAEGERSZEG);
+        } else if(county == "Veszprem"){
+            tower1.setTowerLocation(County::VESZPREM);
+        } else if(county == "Kaposvar"){
+            tower1.setTowerLocation(County::KAPOSVAR);
+        } else if(county == "Pecs"){
+            tower1.setTowerLocation(County::PECS);
+        } else if(county == "Szekszard"){
+            tower1.setTowerLocation(County::SZEKSZARD);
+        } else if(county == "Szekesfehervar"){
+            tower1.setTowerLocation(County::SZEKESFEHERVAR);
+        } else if(county == "Tatabanya"){
+            tower1.setTowerLocation(County::TATABANYA);
+        } else if(county == "Budapest"){
+            tower1.setTowerLocation(County::BUDAPEST);
+        } else if(county == "Kecskemet"){
+            tower1.setTowerLocation(County::KECSKEMET);
+        } else if(county == "Szeged"){
+            tower1.setTowerLocation(County::SZEGED);
+        } else if(county == "Szolnok"){
+            tower1.setTowerLocation(County::SZOLNOK);
+        } else if(county == "Salgotarjan"){
+            tower1.setTowerLocation(County::SALGOTARJAN);
+        } else if(county == "Eger"){
+            tower1.setTowerLocation(County::EGER);
+        } else if(county == "Bekescsaba"){
+            tower1.setTowerLocation(County::BEKESCSABA);
+        } else if(county == "Debrecen"){
+            tower1.setTowerLocation(County::DEBRECEN);
+        } else if(county == "Nyiregyhaza"){
+            tower1.setTowerLocation(County::NYIREGYHAZA);
+        } else if(county == "Miskolc"){
+            tower1.setTowerLocation(County::MISKOLC);
+        } else{
+            tower1.setTowerLocation(County::INVALID);
+        }
+
+        towerVector.push_back(tower1);
+
+    }
+
+    for (int i = 0; i <towerVector.size() ; ++i) {
+        std::cout << i << ":" << towerVector[i].getTowerID() << ", " << towerVector[i].getTowerLocation() << std::endl;
+    }
+
+    file.close();
+    return towerVector;
+}
+
+
+
